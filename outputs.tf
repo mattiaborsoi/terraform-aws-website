cd1173d3523ce08394a75f9e516413acf55adb22
Mattia Borsoi <mattia.borsoi@gmail.com>
2021-02-03 16:23:34 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "ssh_connection_string" {
-  value = "ssh -i \"${var.ssh_private_key_file}\" ubuntu@${aws_spot_instance_request.ec2_instance.public_dns}"
+  value = "ssh -i \"${var.ssh_private_key_file}\" ubuntu@${aws_instance.ec2_instance.public_dns}"
 }
 output "instance_public_ip" {
-  value       = aws_spot_instance_request.ec2_instance.public_ip
+  value       = aws_instance.ec2_instance.public_ip
   description = "The public IP of the web server"
 }
